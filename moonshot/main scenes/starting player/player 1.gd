e12dff310d0ff6116a4940277dc46c03a5c6d0b2
gavin-orrick <74429854+gavin-orrick@users.noreply.github.com>
2020-11-20 09:41:41 -0600

    player movement and screen barrier

--- moonshot/main scenes/starting player/player 1.gd
+++ moonshot/main scenes/starting player/player 1.gd
@@ -4,16 +4,24 @@ var speed: float = 100
 var vel := Vector2(0, 0)
 
 func _process(delta):
+	vel.x= 0
+	vel.y= 0
 	pass
-
 func _physics_process(delta):
 	pass
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("Move_left"):
 		vel.x = -speed
 	elif Input.is_action_pressed("Move_right"):
 		vel.x = speed
-	if Input.is_action_pressed("Move_down"):
-		vel.y = -speed 
-	elif Input.is_action_pressed("Move_up"):
-		vel.y = speed
+	if Input.is_action_pressed("Move_up"):
+		vel.y = -speed
+	elif Input.is_action_pressed("Move_down"):
+		vel.y = speed 
 
+
+	position += vel * delta
+	
+	var viewrect := get_viewport_rect()
+
+	position.x = clamp(position.x,0,viewrect.size.x)
+	position.y = clamp(position.y, 0 ,viewrect.size.y)
